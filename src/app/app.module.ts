5b8d4843b6bee6098bc8ed5ae285f8e0ab609593
davi <daavisouza.santana@gmail.com>
2023-09-18 18:21:52 -0300

    feat: create components basic structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { GameCardComponent } from './components/game-card/game-card.component';
+import { CategoryCardComponent } from './components/category-card/category-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    GameCardComponent,
+    CategoryCardComponent
   ],
   imports: [
     BrowserModule,
